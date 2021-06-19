1f4e3ef2dbe9fa4d4fa2745c88dda92e8f94d89d
Tatyana Shevchenko <t.shevchenko@mafin.ru>
2021-06-19 13:19:49 +0300

    feat(reset and material): add reset.css and material ui

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import Button from '@material-ui/core/Button'
+
+import 'reset-css'
 import './App.css'
 
 function App() {
     return (
         <div className="App">
             App
+            <Button variant="contained" color="primary">
+                Hello World
+            </Button>
         </div>
     )
 }
